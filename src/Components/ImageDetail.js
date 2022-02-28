e0627534539e8846c5015e5d2edce81ab9a1306e
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-28 23:21:08 +0530

    28-02-2022 : React-Native Display ImageScore using props

--- src/Components/ImageDetail.js
+++ src/Components/ImageDetail.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 
-const ImageDetail = ({ title, imageSource }) => {
+const ImageDetail = ({ title, imageSource, score }) => {
   console.log(imageSource);
   return (
     <View>
       <Image source={imageSource} />
       {/* <Image source={require("../../assets/beach.jpg")} /> */}
       <Text>{title}</Text>
+      <Text>Image Scrore - {score}</Text>
     </View>
   );
 };
