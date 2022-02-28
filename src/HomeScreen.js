a888b34744e11c5bb708e49eeeb3b16c90403109
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-28 22:15:57 +0530

    28-02-2022 : Button Example

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return(
-      <Text style={styles.text}>HomeScreen</Text>
-  )
-}
+  return (
+    <View>
+      <Text style={styles.text}>Hi There !</Text>
+      <Button
+        style={styles.button}
+        onPress={() => console.log("Button Pressed")}
+        title="Go to Component Screen"
+      />
+      <TouchableOpacity
+        style={styles.touchable}
+        onPress={() => console.log("List Pressed")}
+      >
+        <Text>Go to List Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    text:{
-        fontSize:30
-    }
-})
+  text: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  touchable: {
+    marginTop: 20,
+  },
+});
 
 export default HomeScreen;
